5d358ba7e73c0e2f2394bf5b1631f1739ec02d32
Ayush Yadav <ayushyadav2706aug@gmail.com>
2026-08-06 12:08:29 +0530

    Time: 1868 ms (5.01%) | Memory: 42.2 MB (20.00%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        for(int i=1;i<=(num/2)+1;i++){
+            if(i*i==num){
+                return true;
+            }
+            if(i*i>num){
+                return false;
+            }
+        }
+        return false;
+        
+
+    }
+}
